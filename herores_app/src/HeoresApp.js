0b28ff423fb871d753352623491ecfc0994ab147
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-15 20:10:06 -0600

    initial commit

---
+++ herores_app/src/HeoresApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeoresApp = () => {
+    return (
+        <div>
+            <h1>Heores App</h1> 
+        </div>
+    )
+}
---
+++ herores_app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HeoresApp } from './HeoresApp';
+
+
+ReactDOM.render(
+  
+    <HeoresApp />,  
+  document.getElementById('root')
+);
