595fcade2e8e3c93baa68c5f7a6e9db699b5ef73
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-15 23:40:54 -0600

    heroScreen and navigation

--- herores_app/src/components/heores/HeroScreen.js
+++ herores_app/src/components/heores/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useParams, Redirect } from "react-router-dom"
 import { getHeroeById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
 
 
     const {heroeId} = useParams();
@@ -18,9 +18,42 @@ export const HeroScreen = () => {
         first_appearance,
         characters,
     }=heroe;
+
+
+    const handleReturn=()=>{
+        if(history.length<=2){
+            history.push("/")
+        }
+        else
+        history.goBack();
+    }
+
+
     return (
-        <div>
-            <h1>Hero Screen</h1>
+        <div className="row mt-5">
+            <div className="col-5">
+                <img 
+                src={`../assets/heroes/${heroeId}.jpg`}
+                alt={superhero}
+                clasname="img-thumbnail"
+                />
+
+            </div>
+            <div className="col-7">
+                <h3>{superhero}</h3>
+                <ul className="list-goup list-group-flush">
+                    <li className="list-group-item"> <b>alter Ego:</b>{alter_ego}</li>
+                    <li className="list-group-item"> <b>publisher:</b>{publisher}</li>
+                    <li className="list-group-item"> <b>first appearance:</b>{first_appearance}</li>
+                </ul>
+                <h5>Characters</h5>
+                <p>{characters}</p>
+                <button className="btn btn-outline-info"
+                        onClick={handleReturn}
+                    >
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
