adaa8e9fa4fda0c00ee9b25a248c34be0751e534
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-17 18:38:07 -0600

    Routes Privated

--- herores_app/src/components/ui/NavBar.js
+++ herores_app/src/components/ui/NavBar.js
@@ -59,7 +59,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <spam className="nav-item nav-link text-info">{name}</spam>
+                    <span className="nav-item nav-link text-info">{name}</span>
                     <button                         
                         className="nav-item nav-link btn" 
                         onClick={handleLogout}
