c88ca38b0b8cbc8c2a9c58e9026aa01b4d1020f0
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-18 20:30:57 -0600

    fix test private route

---
+++ herores_app/src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe('pruebas en <PrivateRoute/>', () => {
+
+    const props = {
+        location:{
+            pathname: '/marvel'
+        }
+    }
+
+    test('debe de mostrar el componente si está autenticado y guardar localStorage', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute 
+                    isAuthenticated={ true }
+                    component={ () => <span>Listo!</span> }
+                    { ...props }
+                />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('span').exists() ).toBe(true);
+        // expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel');
+
+
+    })
+        
+})
