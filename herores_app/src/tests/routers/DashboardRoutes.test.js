b8a3d071935347c5adf294aa1b7bd7dbee131531
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-18 21:57:23 -0600

    test DashBoardRoutes

--- herores_app/src/tests/routers/DashboardRoutes.test.js
+++ herores_app/src/tests/routers/DashboardRoutes.test.js
@@ -1,12 +1,31 @@
 import { DashboardRoutes } from "../../routers/DashboardRoutes";
-import { shallow, mount } from 'enzyme'
-
+import {  mount } from 'enzyme'
+import { AuthContext } from '../../auth/AuthContext';
+import { MemoryRouter } from 'react-router-dom';
 
 
 describe('pruebas en componente <DashBoradRoutes/>', () => {
     
+    const contextValue ={
+        dispatch: jest.fn(),
+        user:{
+            logged:true,
+            name: 'John',
+        }
+    }  
+    
     test('should debe de mostrarse correctamente', () => {
-        const wrapper = mount(<DashboardRoutes/>);
+        const wrapper = mount(
+           
+            <AuthContext.Provider value={contextValue}>
+                 <MemoryRouter>
+                <DashboardRoutes/>
+
+                 </MemoryRouter>
+            </AuthContext.Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find(".text-info").text().trim()).toBe("John");
     })
     
 
