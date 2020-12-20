4d4534c0184c86368fd8bed61949afb47a6815e0
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-19 22:01:13 -0600

    test SearchScreen

---
+++ herores_app/src/tests/components/search/SearchScreen.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter, Route } from 'react-router-dom';
+import { SearchScreen } from '../../../components/search/SearchScreen';
+
+describe('pruebas en <SearchScreen/>', () => {
+
+    test('should de mostrase correctamente con valores por defecto ', () => {
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={["/search"]}>
+                <Route path='/search' component={SearchScreen}>                    
+                </Route>
+            </MemoryRouter>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find(".alert-info").text().trim()).toBe("Search a hero");
+    });
+
+    test('should mostrar a batman y el input con el valor del query String', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={["/search?q=batman"]}>
+                <Route path='/search' component={SearchScreen}>                    
+                </Route>
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find("input").prop("value")).toBe("batman");
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    test('should de mostrar un error si no se encuentrael hero', () => {
+        const heroError="batman123123"
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[`/search?q=${heroError}`]}>
+                <Route path='/search' component={SearchScreen}>                    
+                </Route>
+            </MemoryRouter>
+        );
+        expect(wrapper.find(".alert-danger").text().includes(heroError)).toBe(true);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should de llamar el push del history', () => {
+       const history = {
+           push: jest.fn(),
+       } 
+       const wrapper = mount(
+           <MemoryRouter initialEntries={["/search?q=batman123"]}>
+               <Route path='/search' component={()=><SearchScreen history={history}/>}>                   
+               </Route>
+           </MemoryRouter>
+       );
+
+       wrapper.find("input").simulate("change",{
+            target:{
+                name: "search",
+                value:"batman"
+            }
+       });
+       wrapper.find("form").prop("onSubmit")({preventDefault(){}});
+       expect(history.push).toHaveBeenCalledWith(`?q=batman`);
+
+
+    });
+    
+    
+
+})
